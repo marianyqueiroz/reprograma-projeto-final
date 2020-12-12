4369e85c57989e5fd3765cd634859b13f9205e80
marianyqueiroz <marianyqs@gmail.com>
2020-12-12 20:26:59 -0300

    WIP

--- src/controllers/cursosController.js
+++ src/controllers/cursosController.js
@@ -1,12 +1,30 @@
 const cursos = require("../models/cursos.js");
+const SECRET = process.env.SECRET
+const jwt = require("jsonwebtoken");
 const fs = require("fs");
 
 const getAll = (req, res) => {
-    console.log(req.url);
+    const authHeader = req.get("authorization");
 
-    cursos.find(function (err, cursos){
-        res.status(200).send(cursos);
-    });
+    if (!authHeader){
+        return res.status(401).send("Insira o header!");
+    };
+
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, SECRET, function(erro) {
+        if(erro) {
+            return res.status(403).send("O seu token é inválido!")
+        }
+
+        cursos.find(function (err, cursos){
+            if(err){
+                res.status(500).send({ message : err.message })
+            }
+            res.status(200).send(cursos);
+        });
+    })
+
 };
 
 const postCurso = (req, res) => {
@@ -28,7 +46,7 @@ const postCurso = (req, res) => {
           { $set: req.body },
           { upsert : true},
           function (err) {
-          res.status(201).send({ message: "Curso atualizada com sucesso!" });
+          res.status(201).send({ message: "Curso atualizado com sucesso!" });
       });
     } catch (err) {
       return res.status(424).send({ message: err.message });
