8c13c14c63af5affccd4ec22f85f63e69eb139cf
marianyqueiroz <marianyqs@gmail.com>
2020-12-12 22:14:23 -0300

    WIP

--- src/routes/usuariesRoute.js
+++ src/routes/usuariesRoute.js
@@ -5,6 +5,6 @@ const controller = require("../controllers/usuariesController");
 router.post("/", controller.create);
 router.get("/", controller.getAll);
 router.post("/login", controller.login);
-router.delete("/:_id", controller.deleteUsuarie);
+
 
 module.exports = router;
